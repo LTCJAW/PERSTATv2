5feab08c390bea761e4a0c86cb07dc397ab66234
LTCJAW <james.a.watson72@gmail.com>
2025-01-09 16:26:22 -0500

    added pages and updated menu front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { generateClient } from "aws-amplify/data";
+import EnterStatus from "./pages/EnterStatus";
+import EnterAbsence from "./pages/EnterAbsence";
+import ViewReports from "./pages/ViewReports";
+import AddEmployee from "./pages/AddEmployee";
+import DeleteEmployee from "./pages/DeleteEmployee";
 
-const client = generateClient<Schema>();
 
 function App() {
   const { signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    <Router>
+      <main>
+        <h1>Main Menu</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/enter-status">Enter Today's Personnel Status</Link>
+            </li>
+            <li>
+              <Link to="/enter-absence">Enter Planned Leave or Absence Days</Link>
+            </li>
+            <li>
+              <Link to="/view-reports">View Personnel Status Reports</Link>
+            </li>
+            <li>
+              <Link to="/add-employee">Add a New Employee</Link>
+            </li>
+            <li>
+              <Link to="/delete-employee">Delete an Employee</Link>
+            </li>
+            <li>
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+          </ul>
+        </nav>
+      </main>
+      <Routes>
+        <Route path="/enter-status" element={<EnterStatus />} />
+        <Route path="/enter-absence" element={<EnterAbsence />} />
+        <Route path="/view-reports" element={<ViewReports />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/delete-employee" element={<DeleteEmployee />} />
+        <Route path="/" element={<h2>Welcome to the Main Menu</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
